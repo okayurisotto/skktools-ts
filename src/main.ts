683f9c7881f31e4a5229e2d6b955efd599c2e879
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2022-09-23 17:38:25 +0900

    cleanup(main.ts)

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,6 @@ import {
   sort,
   uniq,
 } from "~/modules/index.ts";
-import { minus } from "~/modules/expr/index.ts";
 import { Args } from "~/structs.ts";
 
 const args = flags.parse(Deno.args);
@@ -26,86 +25,74 @@ const echo = (input: string): void => {
   Deno.writeSync(Deno.stdout.rid, new TextEncoder().encode(input));
 };
 
-const mode = args["mode"];
+const importer = importers[args.from];
+const exporter = exporters[args.to];
 
-(async () => {
-  const importer = importers[args.from];
-  const exporter = exporters[args.to];
+if (args.mode === "convert") {
+  echo(exporter(importer(await stdin())));
+} else if (args.mode === "expr") {
+  echo(exporter(
+    args._
+      .reduce<{
+        operation?: null | "+" | "-";
+        path?: string;
+      }[]>((acc, cur, idx) => {
+        const prev = acc[acc.length - 1];
 
-  if (mode === "convert") {
-    echo(exporter(importer(await stdin())));
-  }
-
-  if (mode === "expr") {
-    echo(exporter(
-      args._
-        .reduce<{
-          operation?: null | "+" | "-";
-          path?: string;
-        }[]>((acc, cur, idx) => {
-          const prev = acc[acc.length - 1];
-
-          if (idx % 2 === 0) {
-            if (prev) {
-              prev.path = cur.toString();
-            } else {
-              acc.push({
-                operation: null,
-                path: cur.toString(),
-              });
-            }
+        if (idx % 2 === 0) {
+          if (prev) {
+            prev.path = cur.toString();
           } else {
-            if (cur !== "+" && cur !== "-") throw Error();
-            acc.push({ operation: cur });
+            acc.push({
+              operation: null,
+              path: cur.toString(),
+            });
           }
+        } else {
+          if (cur !== "+" && cur !== "-") throw Error();
+          acc.push({ operation: cur });
+        }
 
-          return acc;
-        }, [])
-        .map<[null | "+" | "-", Dictionary]>(({ operation, path }) => {
-          if (operation === undefined) throw Error();
-          if (path === undefined) throw Error();
+        return acc;
+      }, [])
+      .map<[null | "+" | "-", Dictionary]>(({ operation, path }) => {
+        if (operation === undefined) throw Error();
+        if (path === undefined) throw Error();
 
-          return [
-            operation,
-            importer(Deno.readTextFileSync(path)),
-          ];
-        })
-        .reduce<Dictionary>((acc, [operation, dict]) => {
-          if (operation === null) return dict;
-          if (operation === "+") return expr.plus(acc, dict);
-          if (operation === "-") return expr.minus(acc, dict);
+        return [
+          operation,
+          importer(Deno.readTextFileSync(path)),
+        ];
+      })
+      .reduce<Dictionary>((acc, [operation, dict]) => {
+        if (operation === null) return dict;
+        if (operation === "+") return expr.plus(acc, dict);
+        if (operation === "-") return expr.minus(acc, dict);
 
-          return acc;
-        }, []),
-    ));
-  }
+        return acc;
+      }, []),
+  ));
+} else if (args.mode === "sort") {
+  echo(exporter(sort(importer(await stdin()))));
+} else if (args.mode === "uniq") {
+  echo(exporter(uniq(importer(await stdin()))));
+} else if (args.mode === "isolate") {
+  const dict = importer(await stdin());
 
-  if (mode === "sort") {
-    echo(exporter(sort(importer(await stdin()))));
-  }
+  const filename = args["filename"];
+  if (filename === undefined) throw Error();
 
-  if (mode === "uniq") {
-    echo(exporter(uniq(importer(await stdin()))));
-  }
+  const hashedEntries = getHashedEntries(dict);
+  Deno.writeTextFileSync(filename + ".hashed", exporter(hashedEntries));
 
-  if (mode === "isolate") {
-    const dict = importer(await stdin());
+  const prefixEntries = getPrefixEntries(dict);
+  Deno.writeTextFileSync(filename + ".prefix", exporter(prefixEntries));
 
-    const filename = args["filename"];
-    if (filename === undefined) throw Error();
+  const suffixEntries = getSuffixEntries(dict);
+  Deno.writeTextFileSync(filename + ".suffix", exporter(suffixEntries));
 
-    const hashedEntries = getHashedEntries(dict);
-    Deno.writeTextFileSync(filename + ".hashed", exporter(hashedEntries));
-
-    const prefixEntries = getPrefixEntries(dict);
-    Deno.writeTextFileSync(filename + ".prefix", exporter(prefixEntries));
-
-    const suffixEntries = getSuffixEntries(dict);
-    Deno.writeTextFileSync(filename + ".suffix", exporter(suffixEntries));
-
-    Deno.writeTextFileSync(
-      filename,
-      exporter(minus(dict, hashedEntries, prefixEntries, suffixEntries)),
-    );
-  }
-})();
+  Deno.writeTextFileSync(
+    filename,
+    exporter(expr.minus(dict, hashedEntries, prefixEntries, suffixEntries)),
+  );
+}

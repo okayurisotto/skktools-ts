109e8f8048d0c20f142caaeb22bd78fed9b2d71a
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2022-09-23 18:39:23 +0900

    update(toJson.ts): 末尾の改行

--- src/exporters/toJson.ts
+++ src/exporters/toJson.ts
@@ -2,6 +2,6 @@ import type { Exporter } from "~/type.ts";
 
 export const toJson: Exporter = (dict) => {
   return new TextEncoder().encode(
-    JSON.stringify(dict, undefined, 2),
+    JSON.stringify(dict, undefined, 2) + "\n",
   );
 };
